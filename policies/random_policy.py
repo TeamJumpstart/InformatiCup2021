a57e703593816418d3c07d9fff19a7946989d19c
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-10-15 17:30:52 +0200

    Comments for RandomPolicy.

--- policies/random_policy.py
+++ policies/random_policy.py
@@ -4,13 +4,19 @@ from policies.policy import Policy
 
 
 class RandomPolicy(Policy):
+    """Policy that chooses action randomly.
+
+    May have different probabilities of different actions.
+
+    Baseline strategy, each smarter policy should be able to outperform this.
+    """
     def __init__(self, seed=None, p=None):
         """Initialize RandomPolicy.
 
         Args:
             seed: Seed for the random number generator. Use a fixed seed for reproducibility,
                   or pass `None` for a random seed.
-            p
+            p: Propability weights of each action. Pass `None` for uniform distribution.
         """
         self.rng = np.random.default_rng(seed)
         if p is not None and len(p) != len(actions):
