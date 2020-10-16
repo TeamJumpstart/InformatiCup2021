df5d3b9a958e39b476eaf0b2f3a6b1c259fbd536
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-10-16 16:53:16 +0200

    Added jumps to policy interface.

--- policies/policy.py
+++ policies/policy.py
@@ -3,13 +3,14 @@ from abc import ABC, abstractmethod
 
 class Policy(ABC):
     @abstractmethod
-    def act(self, cells, you, opponents):
+    def act(self, cells, you, opponents, jumping):
         """Produce an action for a given game state
 
         Args:
-            cells: binary ndarray of cell occupancies. 
+            cells: binary ndarray of cell occupancies.
             you: Controlled player
             opponents: List of other active players
+            jumping: Whether this action will jump
 
         Returns:
             Selected action, one of `environments.spe_ed.directions`
