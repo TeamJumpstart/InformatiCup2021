38c168dc0b8e0777447138e595ae1a1b0f813e97
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2020-10-17 15:42:16 +0200

    updated mazewalker to follow wall

--- policies/mazewalker_policy.py
+++ policies/mazewalker_policy.py
@@ -5,16 +5,40 @@ class MazeWalkerPolicy(Policy):
     """Policy goes straight forward until it hits a wall, then turns.
 
     Baseline strategy, smarter policies should be able to outperform this.
+    
     """
+
+    def __init__(self):
+        """Initialize MazeWalkerPolicy.
+
+        """
+        self.hit_wall = False
+
     def act(self, cells, player, opponents, round):
+                
+        # if_free - relative to player position
+        def is_free(pos):            
+            return cells.is_free(player.position + pos)
+
+        # directions - relative to player direction
+        forward = player.direction.cartesian
+        left = player.direction.turn_left().cartesian
+        right = player.direction.turn_right().cartesian
+        
         # TODO Refactor and improve
-        if cells.is_free(player.position + player.direction.cartesian):
-            return "change_nothing"
 
-        if cells.is_free(player.position + player.direction.turn_left().cartesian):
-            return "turn_left"
-
-        if cells.is_free(player.position + player.direction.turn_right().cartesian):
-            return "turn_right"
+        if self.hit_wall: # follow the wall
+            if is_free(left):
+                return "turn_left"
+            elif is_free(forward):
+                return "change_nothing"
+            elif is_free(right):
+                return "turn right"
+        else: # search for the wall
+            if is_free(forward):
+                return "change_nothing"
+            else:
+                self.hit_wall = True
+                return "turn_right"
 
         return "change_nothing"  # We're surrounded
