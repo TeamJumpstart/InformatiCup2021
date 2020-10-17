fbc7feb95740f2c4d07df5032d64b92d62d6c789
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-10-17 12:40:50 +0200

    Renamed round to rounds.

--- policies/policy.py
+++ policies/policy.py
@@ -3,14 +3,14 @@ from abc import ABC, abstractmethod
 
 class Policy(ABC):
     @abstractmethod
-    def act(self, cells, you, opponents, round):
+    def act(self, cells, you, opponents, rounds):
         """Produce an action for a given game state
 
         Args:
             cells: binary ndarray of cell occupancies.
             you: Controlled player
             opponents: List of other active players
-            round: Number of this round. Starts with 1, thus `round % 6 == 0` indicates a jump.
+            rounds: Number of this round. Starts with 1, thus `rounds % 6 == 0` indicates a jump.
 
         Returns:
             Selected action, one of `environments.spe_ed.directions`
