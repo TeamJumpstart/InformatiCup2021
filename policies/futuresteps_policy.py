8c9c364f3067d58b2d684d4f5f23f39da7a2e614
GregorKobsik <gregor.kobsik@gmail.com>
2020-10-20 14:10:22 +0200

    FutureStepsPolicy: updated code for dynamic behaviour, probably the same error as fixed in RandomProbingPolicy.

--- policies/futuresteps_policy.py
+++ policies/futuresteps_policy.py
@@ -25,16 +25,16 @@ class FutureStepsPolicy(Policy):
         self.rng = np.random.default_rng(seed)
 
     def act(self, cells, player, opponents, round):
-        def sum_future_steps(pos, direction, n_steps, future_steps=None):
+        def sum_future_steps(pos, direction, n_steps, steps=[]):
             number_of_moves = 0
             pos = pos + direction.cartesian  # update one position step
 
             if cells.is_free(pos):  # check current pos
                 if self.dynamic:  # check dynamic board state with future moves
-                    if np.any(future_steps == pos):
+                    if tuple(pos) in steps:
                         return number_of_moves
                     else:
-                        np.append(future_steps, pos, axis=-1)
+                        steps = steps + [tuple(pos)]
                         number_of_moves += 1
                 else:  # assume a static board state
                     number_of_moves += 1  # valid move, add 1
@@ -45,19 +45,19 @@ class FutureStepsPolicy(Policy):
                 return number_of_moves  # reached n_steps in future, return
             else:
                 # try to move left, add num of possible future steps
-                number_of_moves += sum_future_steps(pos, direction.turn_left(), n_steps - 1, future_steps)
+                number_of_moves += sum_future_steps(pos, direction.turn_left(), n_steps - 1, steps)
                 # try to move forward, add num of possible future steps
-                number_of_moves += sum_future_steps(pos, direction, n_steps - 1, future_steps)
+                number_of_moves += sum_future_steps(pos, direction, n_steps - 1, steps)
                 # try to move right, add num of possible future steps
-                number_of_moves += sum_future_steps(pos, direction.turn_right(), n_steps - 1, future_steps)
+                number_of_moves += sum_future_steps(pos, direction.turn_right(), n_steps - 1, steps)
                 return number_of_moves  # tried all directions, return
 
             return 0  # end sum_future_steps
 
         # Compute number of steps after `n_steps` in the future given an initial direction
-        sum_left = sum_future_steps(player.position, player.direction.turn_left(), self.n_steps, [])
-        sum_forward = sum_future_steps(player.position, player.direction, self.n_steps, [])
-        sum_right = sum_future_steps(player.position, player.direction.turn_right(), self.n_steps, [])
+        sum_left = sum_future_steps(player.position, player.direction.turn_left(), self.n_steps)
+        sum_forward = sum_future_steps(player.position, player.direction, self.n_steps)
+        sum_right = sum_future_steps(player.position, player.direction.turn_right(), self.n_steps)
 
         # Choose action based on number of possible future steps.
         if (sum_left > sum_forward) & (sum_left >= sum_right):
