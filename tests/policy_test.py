0dc3e8185f1db402697cdff3f4e9075dab94f017
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-10-22 17:00:45 +0200

    Simple unittests for policies.

--- tests/policy_test.py
+++ tests/policy_test.py
@@ -1,11 +1,75 @@
 import unittest
-from policies import ScriptedPolicy
+from environments import SimulatedSpe_edEnv
+import policies
+
+
+def run_policy(env, pol):
+    obs = env.reset()
+    done = False
+    while not done:
+        action = pol.act(*obs)
+        obs, _, done, _ = env.step(action)
+
+
+class TestCirclePolicy(unittest.TestCase):
+    def test_execution(self):
+        """Executing the policy should not throw any error."""
+        env = SimulatedSpe_edEnv(5, 5, [policies.CirclePolicy() for _ in range(5)])
+        pol = policies.CirclePolicy()
+        run_policy(env, pol)
+
+
+class TestFutureStepsPolicy(unittest.TestCase):
+    def test_execution(self):
+        """Executing the policy should not throw any error."""
+        env = SimulatedSpe_edEnv(5, 5, [policies.FutureStepsPolicy() for _ in range(5)])
+        pol = policies.FutureStepsPolicy()
+
+        run_policy(env, pol)
+
+    def test_execution_dynamic(self):
+        """Executing the policy should not throw any error."""
+        env = SimulatedSpe_edEnv(5, 5, [policies.FutureStepsPolicy(dynamic=True) for _ in range(5)])
+        pol = policies.FutureStepsPolicy(dynamic=True)
+
+        run_policy(env, pol)
+
+
+class TestMazeWalkerPolicy(unittest.TestCase):
+    def test_execution(self):
+        """Executing the policy should not throw any error."""
+        env = SimulatedSpe_edEnv(5, 5, [policies.MazeWalkerPolicy() for _ in range(5)])
+        pol = policies.MazeWalkerPolicy()
+        run_policy(env, pol)
+
+
+class TestRandomPolicy(unittest.TestCase):
+    def test_execution(self):
+        """Executing the policy should not throw any error."""
+        env = SimulatedSpe_edEnv(5, 5, [policies.RandomPolicy() for _ in range(5)])
+        pol = policies.RandomPolicy()
+        run_policy(env, pol)
+
+
+class TestRandomProbingPolicy(unittest.TestCase):
+    def test_execution(self):
+        """Executing the policy should not throw any error."""
+        env = SimulatedSpe_edEnv(5, 5, [policies.RandomProbingPolicy() for _ in range(5)])
+        pol = policies.RandomProbingPolicy()
+        run_policy(env, pol)
 
 
 class TestScriptedPolicy(unittest.TestCase):
+    def test_execution(self):
+        """Executing the policy should not throw any error."""
+        env = SimulatedSpe_edEnv(5, 5, [policies.ScriptedPolicy([]) for _ in range(5)])
+        pol = policies.ScriptedPolicy(["turn_left", "turn_right", "slow_down", "speed_up", "change_nothing"])
+
+        run_policy(env, pol)
+
     def test_action_sequence(self):
         """Returns planned actions for the corresponding rounds."""
-        pol = ScriptedPolicy(["turn_left", "turn_right", "slow_down", "speed_up", "change_nothing"])
+        pol = policies.ScriptedPolicy(["turn_left", "turn_right", "slow_down", "speed_up", "change_nothing"])
 
         self.assertEqual(pol.act(None, None, None, 1), "turn_left")
         self.assertEqual(pol.act(None, None, None, 2), "turn_right")
@@ -15,7 +79,7 @@ class TestScriptedPolicy(unittest.TestCase):
 
     def test_repeatability(self):
         """Action only depends on the round and not on internal state."""
-        pol = ScriptedPolicy(["turn_left", "turn_right", "slow_down", "speed_up", "change_nothing"])
+        pol = policies.ScriptedPolicy(["turn_left", "turn_right", "slow_down", "speed_up", "change_nothing"])
 
         self.assertEqual(pol.act(None, None, None, 1), "turn_left")
         self.assertEqual(pol.act(None, None, None, 1), "turn_left")
@@ -24,7 +88,7 @@ class TestScriptedPolicy(unittest.TestCase):
 
     def test_run_out(self):
         """Should return `"change_nothing"` after finishing the planned actions."""
-        pol = ScriptedPolicy(["slow_down"])
+        pol = policies.ScriptedPolicy(["slow_down"])
 
         self.assertEqual(pol.act(None, None, None, 1), "slow_down")
         self.assertEqual(pol.act(None, None, None, 2), "change_nothing")
@@ -33,6 +97,14 @@ class TestScriptedPolicy(unittest.TestCase):
 
     def test_empty_plan(self):
         """Can handle an empoty action sequence."""
-        pol = ScriptedPolicy([])
+        pol = policies.ScriptedPolicy([])
 
         self.assertEqual(pol.act(None, None, None, 1), "change_nothing")
+
+
+class TestSpiralPolicy(unittest.TestCase):
+    def test_execution(self):
+        """Executing the policy should not throw any error."""
+        env = SimulatedSpe_edEnv(5, 5, [policies.SpiralPolicy() for _ in range(5)])
+        pol = policies.SpiralPolicy()
+        run_policy(env, pol)
