f5c9554ffa15618f51b07b46679ff6bd0802c564
Gregor Kobsik <gregor.kobsik@gmail.com>
2020-10-25 22:57:22 +0100

    RandomProbing: uses now directly the simulator the simulate steps - allows now to utilise full action set.

--- policies/randomprobing_policy.py
+++ policies/randomprobing_policy.py
@@ -1,49 +1,57 @@
 import numpy as np
 from policies.policy import Policy
+from environments.simulator import Spe_edSimulator
 
 
 class RandomProbingPolicy(Policy):
     """Policy that performs `n_probe` random runs to check whether
     the agent will survive for `n_steps` within the current cell state.
 
-    TODO: extend by `probing_policy` argument, which defines the behaviour for the probe runs.
-
     Baseline strategy, each smarter policy should be able to outperform this.
     """
-    def __init__(self, n_steps=10, n_probes=3, seed=None):
+    def __init__(self, n_steps=3, n_probes=10, full_action_set=False, seed=None):
         """Initialize RandomProbingPolicy.
 
         Args:
             n_steps: Defines the number of steps each probe run is performed at most.
             n_probes: Defines the number of probe runs per available `p_action`.
+            full_action_set: `True`, selects one of all available actions:
+                ("change_nothing", "turn_left", "turn_right", "speed_up", "slow_down"),
+                `False`, selects one of the following actions:
+                ("change_nothing", "turn_left", "turn_right").
             seed: Seed for the random number generator. Use a fixed seed for reproducibility,
                   or pass `None` for a random seed.
         """
 
         self.n_steps = n_steps
         self.n_probes = n_probes
+        self.full_action_set = full_action_set
         self.rng = np.random.default_rng(seed)
 
     def act(self, cells, player, opponents, rounds):
         """Chooses action based on random probe runs."""
 
-        actions = np.array(["change_nothing", "turn_left", "turn_right"])
-        directions = np.array([player.direction, player.direction.turn_left(), player.direction.turn_right()])
+        if self.full_action_set:
+            actions = np.array(["change_nothing", "turn_left", "turn_right", "speed_up", "slow_down"])
+        else:
+            actions = np.array(["change_nothing", "turn_left", "turn_right"])
         sum_actions = np.zeros(actions.shape)
 
-        def perform_probe_run(pos, direction, n_steps, steps=[]):
+        def perform_probe_run(action, n_steps):
             """Performs one recursive probe run with random actions and returns the number of steps survived."""
-            pos = pos + direction.cartesian
-            if cells.is_free(pos) and (not np.any(steps == pos)) and (n_steps > 0):
-                steps = steps + [pos]
-                action = self.rng.choice(directions)
-                return 1 + perform_probe_run(pos, action, n_steps - 1, steps)
-            else:
-                return 0
+            env = Spe_edSimulator(cells.cells, [player], rounds).step([action])
 
-        # perform 3 * `n_probes` runs each with maximum of `n_steps`
+            while env.players[0].active and (n_steps > 0):
+                n_steps -= 1
+                action = self.rng.choice(actions)
+                env = env.step([action])
+
+            return self.n_steps - n_steps
+
+        # perform 3 or 5 * `n_probes` runs each with maximum of `n_steps`
         for _ in range(self.n_probes):
-            for d, direction in enumerate(directions):
-                sum_actions[d] += perform_probe_run(player.position, direction, self.n_steps)
+            for a, action in enumerate(actions):
+                sum_actions[a] += perform_probe_run(action, self.n_steps)
 
+        print(sum_actions)
         return actions[np.argmax(sum_actions)]
