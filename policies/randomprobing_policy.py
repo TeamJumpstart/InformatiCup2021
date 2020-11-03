4f35dd6b7672357f096053e7f3b9c1c622e8d39d
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-11-03 15:27:13 +0100

    Added region size awareness to RandomProbingPolicy.

--- policies/randomprobing_policy.py
+++ policies/randomprobing_policy.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import ndimage
 from policies.policy import Policy
 from environments.simulator import Spe_edSimulator
 
@@ -67,10 +68,29 @@ class RandomProbingPolicy(Policy):
 
             return env.rounds  # Return number of survived steps
 
+        def region_size(action):
+            """Compute the of the region we're in after taking action."""
+            sim = Spe_edSimulator(cells.cells, [player], rounds).step([action])
+            if not sim.players[0].active:
+                return 0
+
+            empty = sim.cells == 0
+            empty[sim.players[0].y, sim.players[0].x] = True  # Clear cell we're in
+            labelled, _ = ndimage.label(empty)
+            region = labelled[sim.players[0].y, sim.players[0].x]  # Get the region we're in
+            region_size = np.sum(labelled == region)
+            return region_size
+
         # perform 3 or 5 * `n_probes` runs each with maximum of `n_steps`
-        for _ in range(self.n_probes):
-            for a, action in enumerate(actions):
+        for a, action in enumerate(actions):
+            for _ in range(self.n_probes):
                 sum_actions[a] = max(perform_probe_run([action], self.n_steps), sum_actions[a])
-        sum_actions += self.rng.uniform(0, 0.1, size=sum_actions.shape)  # Add tie-breaker
+                if sum_actions[a] > rounds + self.n_steps:
+                    break  # Maximal path is found, no need in searching further
+
+            # Add region size
+            sum_actions[a] += region_size(action) / (cells.width * cells.height)  # Normalize region size
+
+        sum_actions += self.rng.uniform(0, 1 / (cells.width * cells.height), size=sum_actions.shape)  # Add tie-breaker
 
         return actions[np.argmax(sum_actions)]
