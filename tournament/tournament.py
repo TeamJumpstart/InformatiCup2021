d42b57aa1aeda3f1da1e02bc7c1f580a8c100a89
ManuelPozor <manuel.pozor@rwth-aachen.de>
2020-11-07 20:36:30 +0100

    fixed path issues

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,12 +1,54 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools as it
 from tqdm import tqdm
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from environments import SimulatedSpe_edEnv
 from environments.logging import Spe_edLogger
-from policies import *
+from policies import RandomPolicy, HeuristicPolicy, SpiralPolicy
 from heuristics import PathLengthHeuristic, RandomHeuristic, CompositeHeuristic, RegionHeuristic, OpponentDistanceHeuristic
 import logging
+
+pol = HeuristicPolicy(
+    CompositeHeuristic(
+        [
+            PathLengthHeuristic(20, 100),
+            RegionHeuristic(),
+            OpponentDistanceHeuristic(dist_threshold=16),
+            RandomHeuristic(),
+        ],
+        weights=[20, 1, 1e-3, 1e-4]
+    )
+)
+POLICY_LIST = [
+    {
+        "name": "Composite",
+        "pol": pol
+    },
+    {
+        "name": "Random",
+        "pol": RandomPolicy()
+    },
+    {
+        "name": "Spiral",
+        "pol": SpiralPolicy()
+    },
+    {
+        "name": "PathLength",
+        "pol": HeuristicPolicy(PathLengthHeuristic())
+    },
+    {
+        "name": "Region",
+        "pol": HeuristicPolicy(RegionHeuristic())
+    },
+    {
+        "name": "OppDist",
+        "pol": HeuristicPolicy(OpponentDistanceHeuristic())
+    },
+]
+number_games = 1
 
 # Set up logging
 logging.basicConfig(
@@ -16,7 +58,7 @@ logging.basicConfig(
 )
 
 
-def play(env, pol, show=False, render_file=None, fps=10, logger=None, silent=False):
+def play_game(env, policy, show=False, fps=10, logger=None):
     obs = env.reset()
 
     if show and not env.render(screen_width=720, screen_height=720):
@@ -25,16 +67,14 @@ def play(env, pol, show=False, render_file=None, fps=10, logger=None, silent=Fal
         states = [env.game_state()]
 
     done = False
-    with tqdm(disable=not silent) as pbar:
-        while not done:
-            action = pol.act(*obs)
-            obs, reward, done, _ = env.step(action)
+    while not done:
+        action = policy["pol"].act(*obs)
+        obs, reward, done, _ = env.step(action)
 
-            if show and not env.render(screen_width=720, screen_height=720):
-                return
-            if logger is not None:
-                states.append(env.game_state())
-            pbar.update()
+        if show and not env.render(screen_width=720, screen_height=720):
+            return
+        if logger is not None:
+            states.append(env.game_state())
 
     if logger is not None:
         logger.log(states)
@@ -47,7 +87,7 @@ def play(env, pol, show=False, render_file=None, fps=10, logger=None, silent=Fal
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='spe_ed')
+    parser = argparse.ArgumentParser(description='spe_ed tournament')
     parser.add_argument('--show', action='store_true', help='Display game.')
     parser.add_argument('--log-dir', type=str, default=None, help='Directory for logs.')
     args = parser.parse_args()
@@ -59,25 +99,15 @@ if __name__ == "__main__":
     else:
         logger = None
 
-    # Create environment
-    env = SimulatedSpe_edEnv(40, 40, [HeuristicPolicy(PathLengthHeuristic()) for _ in range(5)])
+    # ToDo: create varying with, height, number of players and constellations
 
-    # Create policy to test
-    pol = HeuristicPolicy(
-        CompositeHeuristic(
-            [
-                PathLengthHeuristic(20, 100),
-                RegionHeuristic(),
-                OpponentDistanceHeuristic(dist_threshold=16),
-                RandomHeuristic(),
-            ],
-            weights=[20, 1, 1e-3, 1e-4]
-        )
-    )
+    with tqdm() as pbar:
+        for number_players in range(2, 7):  # games with 2 to 6 players
+            player_constellations = it.combinations(POLICY_LIST, number_players)  # maybe with replacements
+            for constellation in player_constellations:
+                # Create environment
+                env = SimulatedSpe_edEnv(40, 40, [c["pol"] for c in constellation[1:]])
+                for game in range(number_games):
+                    play_game(env, constellation[0], show=args.show)
 
-    repeat = not args.show
-
-    while True:
-        play(env, pol, show=args.show, render_file=args.render_file, fps=args.fps, logger=logger, silent=not repeat)
-        if not repeat:
-            break
+            pbar.update()
