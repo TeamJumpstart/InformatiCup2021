2722d3405434808557f3aa1cddeb6c85adb6b743
ManuelPozor <manuel.pozor@rwth-aachen.de>
2020-11-18 20:59:32 +0100

    integrated tournament into main

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,13 +1,10 @@
-import argparse
-import numpy as np
 import matplotlib.pyplot as plt
 import itertools as it
 from tqdm import tqdm
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from environments import SimulatedSpe_edEnv
+from pathlib import Path
+from environments.simulator import SimulatedSpe_edEnv
 from environments.logging import TournamentLogger
-import tournament_config
+import tournament.tournament_config
 
 
 def play_game(env, policies, show=False, fps=10, logger=None):
@@ -40,45 +37,38 @@ def play_game(env, policies, show=False, fps=10, logger=None):
             plt.pause(0.01)  # Sleep
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='spe_ed tournament')
-    parser.add_argument('mode', nargs='?', choices=[
-        'play',
-        'analyze',
-    ], default="play")
-    parser.add_argument('--show', action='store_true', help='Display game.')
-    parser.add_argument('--log-dir', type=str, default=None, help='Directory for logs.')
-    args = parser.parse_args()
-
+def run_tournament(show, log_dir):
     # Create logger
-    if args.log_dir is not None:
-        logger = TournamentLogger(args.log_dir)
+    if log_dir is not None:
+        directory = Path(log_dir)
+        if not directory.is_dir():
+            directory.mkdir(parents=True, exist_ok=True)
+        logger = TournamentLogger(log_dir)
     else:
         logger = None
 
-    if args.mode == "analyze":
-        # ToDo
-        pass
-    else:  # play
-        # games with 2 to 6 players
-        with tqdm(total=5, desc="Number of players(2-6)", position=0) as player_number_pbar:
-            for tournament_config.number_players in range(2, 7):
-                player_constellations = list(
-                    it.combinations(tournament_config.policy_list, tournament_config.number_players)
-                )  # maybe with replacements
-                # games with different policy combinations
-                with tqdm(
-                    total=len(player_constellations),
-                    desc="Combinations",
-                    position=tournament_config.number_players - 1
-                ) as constellation_pbar:
-                    for constellation in player_constellations:
-                        # games with different map size
-                        for (width, height) in tournament_config.width_height_pairs:
-                            # Create environment
-                            env = SimulatedSpe_edEnv(width, height, [c["pol"] for c in constellation[1:]])
-                            # number of games to be played
-                            for game in range(tournament_config.number_games):
-                                play_game(env, constellation, show=args.show, logger=logger)
-                        constellation_pbar.update()
-                player_number_pbar.update()
+    # games with 2 to 6 players
+    with tqdm(total=5, desc="Number of players(2-6)", position=0) as player_number_pbar:
+        for tournament.tournament_config.number_players in range(2, 7):
+            player_constellations = list(
+                it.combinations(tournament.tournament_config.policy_list, tournament.tournament_config.number_players)
+            )  # maybe with replacements
+            # games with different policy combinations
+            with tqdm(
+                total=len(player_constellations),
+                desc="Combinations",
+                position=tournament.tournament_config.number_players - 1
+            ) as constellation_pbar:
+                for constellation in player_constellations:
+                    # games with different map size
+                    for (width, height) in tournament.tournament_config.width_height_pairs:  # ToDo: add to file name
+                        # do not run games when log already exists
+                        log_file = directory / "_".join([pol_name for pol_name in dict(constellation).keys()]) / ".json"
+                        if logger is not None and log_file.is_file():
+                            continue
+                        env = SimulatedSpe_edEnv(width, height, [c["pol"] for c in constellation[1:]])
+                        # number of games to be played
+                        for game in range(tournament.tournament_config.number_games):
+                            play_game(env, constellation, show=show, logger=logger)
+                    constellation_pbar.update()
+            player_number_pbar.update()
