a303de918e641251f0f62fac902f5333d776cc4c
Robin Kupper <robin.kupper@rwth-aachen.de>
2021-01-05 19:17:23 +0100

    Implemented DFS based PathLengthHeuristic.

--- heuristics/pathlength_heuristic.py
+++ heuristics/pathlength_heuristic.py
@@ -1,49 +1,49 @@
 from heuristics.heuristic import Heuristic
-import numpy as np
 from environments.simulator import Spe_edSimulator
-from environments import spe_ed
 
 
 class PathLengthHeuristic(Heuristic):
     """Performs a random probe run and evaluates length of the path."""
-    def __init__(self, n_steps=20, n_probes=100, seed=None):
-        """Initialize PathLengthHeuristic."""
+    def __init__(self, n_steps, n_probes=None, seed=None):
+        """Initialize PathLengthHeuristic.
+
+        Args:
+            n_steps: Number of steps to look into the future
+        """
         self.n_steps = n_steps
-        self.n_probes = n_probes
-        self.rng = np.random.default_rng(seed)
 
     def score(self, cells, player, opponents, rounds):
-        """Performs probe runs with random actions and returns the number of steps survived."""
-        def perform_probe_run(env):
-            """Simulate the given environment for maximum of `n_steps` with valid random steps or
-            until the player cannot make a valid move, return the environment.
-            """
-            for _ in range(self.n_steps):
-                dead_end = True
-                for action in self.rng.permutation(spe_ed.actions):
-                    env = env.step([action])
-                    if env.players[0].active:
-                        # We survive, go to next step
-                        dead_end = False
-                        break
-                    else:
-                        env = env.undo()  # We die, try alternative action
-                if dead_end:  # No way out
-                    break
-            return env
+        """Perform a DFS to seach the longest path reachable."""
+        expanded = 0
 
-        probe_length = 0
-        for _ in range(self.n_probes):
-            # perform a single probe run
-            env = perform_probe_run(Spe_edSimulator(cells, [player], rounds))
-            # remember only the length of the best probe run
-            probe_length = max(env.rounds - rounds, probe_length)
-            # early out - we found one path with the maximal distance
-            if probe_length >= self.n_steps:
-                return 1.0
+        def _dfs(sim):
+            """Depth-first search"""
+            nonlocal expanded
+
+            path_length = sim.rounds - rounds
+            if path_length >= self.n_steps or expanded > 1000:  # Maximum search depth reached
+                return path_length  # Early out
+
+            for action in ("change_nothing", "turn_left", "turn_right", "slow_down", "speed_up"):
+                sub_sim = sim.step([action])
+                if not sub_sim.player.active:  # Backtrack
+                    continue
+
+                sub_path_length = _dfs(sub_sim)
+                if sub_path_length >= self.n_steps:  # Maximum search depth reached
+                    return sub_path_length  # Early out
+
+                if sub_path_length > path_length:
+                    path_length = sub_path_length
+
+            expanded += 1  # Count expanded nodes
+
+            return path_length
+
+        path_length = _dfs(Spe_edSimulator(cells, [player], rounds))
 
         # return the board state score value
-        return probe_length / self.n_steps
+        return path_length / self.n_steps
 
     def __str__(self):
         """Get readable representation."""
