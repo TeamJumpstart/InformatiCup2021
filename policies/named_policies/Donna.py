6220104bfc0a9c2a7bf99512c45616c752d6a19d
Robin Kupper <robin.kupper@rwth-aachen.de>
2021-01-15 23:17:25 +0100

    Donna

---
+++ policies/named_policies/Donna.py
@@ -0,0 +1,14 @@
+from policies import ActionSearchPolicy
+from heuristics import CompositeHeuristic, PathLengthHeuristic, RegionHeuristic
+
+pol = ActionSearchPolicy(
+    CompositeHeuristic(
+        [
+            PathLengthHeuristic(15),
+            RegionHeuristic(),
+        ],
+        weights=[15, 1],
+    ),
+    depth_limit=6,
+    expanded_node_limit=100,
+)
