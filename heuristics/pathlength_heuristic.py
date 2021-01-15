0fff3bf98b8e957067896f23a834b1b5afe47ca5
Robin Kupper <robin.kupper@rwth-aachen.de>
2021-01-15 15:22:37 +0100

    Occ to pathlength

--- heuristics/pathlength_heuristic.py
+++ heuristics/pathlength_heuristic.py
@@ -1,5 +1,8 @@
+from math import prod
+from operator import itemgetter
 from heuristics.heuristic import Heuristic
 from environments.simulator import Spe_edSimulator
+from state_representation import occupancy_map
 
 
 class PathLengthHeuristic(Heuristic):
@@ -17,20 +20,31 @@ class PathLengthHeuristic(Heuristic):
         """Perform a DFS to seach the longest path reachable."""
         expanded = 0
 
-        def _dfs(sim):
+        occ = occupancy_map(cells, opponents, rounds, depth=2)
+
+        def _dfs(sim, freeness=1):
             """Depth-first search"""
             nonlocal expanded
 
-            path_length = sim.rounds - rounds
-            if path_length >= self.n_steps or expanded > self.expanded_node_limit:  # Maximum search depth reached
+            path_length = (sim.rounds - rounds) * freeness
+            if sim.rounds - rounds >= self.n_steps or expanded > self.expanded_node_limit:  # Maximum search depth reached
                 return path_length  # Early out
 
+            states = []
             for action in ("change_nothing", "turn_left", "turn_right", "slow_down", "speed_up"):
                 sub_sim = sim.step([action])
                 if not sub_sim.player.active:  # Backtrack
                     continue
 
-                sub_path_length = _dfs(sub_sim)
+                # Compute cumulative occupancy of newly ossupied cells
+                sub_freeness = freeness * prod(1 - occ[cell[1], cell[0]] for cell in sub_sim.changed)
+                states.append((sub_sim, sub_freeness))
+
+            # Sort by freeness, higesst freeness first
+            states.sort(key=itemgetter(1), reverse=True)
+
+            for sub_sim, sub_freeness in states:
+                sub_path_length = _dfs(sub_sim, sub_freeness)
                 if sub_path_length >= self.n_steps:  # Maximum search depth reached
                     return sub_path_length  # Early out
 
