f3bd56130a5256ba482bc56c4aea526884a14610
GregorKobsik <gregor.kobsik@gmail.com>
2021-01-17 20:23:17 +0100

    fix bug with min in nearest opp distance

--- heuristics/conditions/nearestopponentdistance_condition.py
+++ heuristics/conditions/nearestopponentdistance_condition.py
@@ -18,6 +18,9 @@ class NearestOpponentDistanceCondition(Condition):
     def score(self, cells, player, opponents, rounds, deadline):
         """Compute the relative size of the region we're in."""
         # close all 1 cell wide openings aka "articulating points"
+        if not player.active:
+            return 0
+
         if self.opening_iterations:
             cells = morphology.binary_opening(cells, iterations=self.opening_iterations)
 
@@ -35,14 +38,19 @@ class NearestOpponentDistanceCondition(Condition):
         # Get the region each player is in
         player_regions = np.array([labelled[p.y, p.x] for p in players])
 
+        # check if we are the only one in region
+        if sum(player_regions == player_regions[0]) < 2:
+            return float("Inf")
+
         def dist(pos1, pos2):
             return sum(np.abs(pos1 - pos2))
 
+        # compute the distance to the nearest opponent in our region
         min_distance = min(
-            dist(player.position, p.position) for p in opponents if labelled[p.y, p.x] == player_regions[0]
+            dist(player.position, o.position) for o in opponents if labelled[o.y, o.x] == player_regions[0]
         )
 
-        # sum player region size divided by the board size, score in [0..1]
+        # return minimal distance
         return min_distance
 
     def __str__(self):
